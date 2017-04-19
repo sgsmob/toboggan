79fe4687ba6f8b88d14f1c20660c40f0440c58d5
Mike O'Brien <sgsmob@gmail.com>
2017-04-19 17:12:50 -0400

    backpointer traversal in DP table

--- toboggan/dp.py
+++ toboggan/dp.py
@@ -7,7 +7,7 @@
 # python libs
 import time
 from operator import itemgetter
-from collections import defaultdict
+from collections import defaultdict, deque
 
 # local imports
 from toboggan.flow import Instance, Constr, SolvedConstr, PathConf
@@ -128,4 +128,77 @@ def solve(instance, silent=True, guessed_weights=None):
 
     candidates = new_table[PathConf.init(n-1, allpaths)]
     return candidates
+
+
+def solve_and_recover(instance, weights, silent=True):
+    dpgraph = instance.dpgraph
+    k = instance.k
+    n = instance.n
+
+    if not silent:
+        print(dpgraph)
+
+    # since we know all the weights, we can stored them as a solved constraint
+    # system
+    globalconstr = SolvedConstr(weights, instance)
+
+    assert len(weights) == k
+    allpaths = frozenset(range(k))
+
+    # Build DP table, which will map path configurations to the path
+    # configurations in the previous tables that yielded the specific entry
+    # All k paths start at the source vertex
+    initial_entries = {PathConf.init(0, allpaths): None}
+    backptrs = [initial_entries]
+
+    # Run DP
+    for i in range(n-1):
+        if not silent:
+            print("")
+            print("Active ({}): {}".format(i, instance.cuts[i]))
+            print("Removing {} from active set".format(i))
+
+        entries = {}
+        for old_paths in backptrs[-1].keys():
+            # Distribute paths incoming to i onto its neighbors
+            if not silent:
+                print("  Pushing {}".format(old_paths))
+            for new_paths, dist in old_paths.push(i, dpgraph[i]):
+                # make sure the sets of paths that were pushed along each
+                # edge sum to the proper flow value.  If not, don't create a
+                # new table entry for this path set.
+                for e, P in dist:  # Paths P coincide on edge e
+                    success = globalconstr.add_constraint(P, e)
+                    if success is None:
+                        break
+                else:
+                    entries[new_paths] = old_paths
+
+        # add the new entries to the list of backpointers
+        backptrs.append(entries)
+
+    if not silent:
+        print("\nDone.")
+        print(backptrs[n-1])
+
+    # recover the paths
+    full_paths = [deque() for _ in weights]
+    try:
+        # the "end" of the DP is all paths passing through the sink
+        conf = PathConf.init(n-1, allpaths)
+        # iterate over the backpointer list in reverse
+        for table in reversed(backptrs):
+            for v, incidence in conf:
+                # vertices might repeat in consecutive table entries if an edge
+                # is "long" wrt the topological ordering.  Don't add it twice
+                # to the path lists in this case.
+                for p in incidence:
+                    if len(full_paths[p]) == 0 or full_paths[p][-1] != v:
+                        full_paths[p].appendleft(v)
+            # traverse the pointer backwards
+            conf = table[conf]
+    except ValueError as e:
+        raise Exception("The set of weights is not a valid solution") from e
+    
+    return full_paths
 
