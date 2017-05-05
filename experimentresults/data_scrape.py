edc8f5eb7c28508b427c73755a3f902e1bbcd574
kkloste <kyle.kloster@gmail.com>
2017-05-05 11:36:36 -0400

    edits to post-processor, also data from zebra+human experiments

--- experimentresults/data_scrape.py
+++ experimentresults/data_scrape.py
@@ -1,4 +1,18 @@
+"""
+Given an output file from run_experiment.py, this script sifts through
+the results and reports analysis such as average runtime.
+
+Use:
+    python3 data_scrape.py [experimentresults file] [new file to print out to]
+
+    - the experimentresults file should contain output from run_experiments.py
+    - the new file will contain a list of instances that timed out
+"""
+
 import sys
+import matplotlib
+# This line prevents an error arising when running this via ssh
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from collections import Counter
 
@@ -25,12 +39,6 @@ time_out = []
 weights = []
 
 time_flag = 0
-
-
-
-
-
-
 
 
 #print "reading in edgelist"
@@ -80,21 +88,16 @@ writer = open(timeout_file, 'w')
 for t_f, t_i, t_n in timed_out_graphs:
 	writer.write("%s %s %s\n" % (t_f, t_i, t_n))
 
-print("Timed out graphs written to %s" % (timeout_file))
+print("Total num instances is {}".format(len(k_opt)))
 
-#scan for any instances where k_optimal != k_groundtruth
-print("(1) Instances where k-optimal != k-groundtruth:")
-for i in range(len(k_opt)):
-	if k_opt[i] != k_gtrue[i] and time_out[i] == 0:
-		print(name[i], k_opt[i], k_gtrue[i])
+print("{} timed out graphs written to {}".format(len(timed_out_graphs), timeout_file))
+
 
 #plot runtime (time_weights + time_paths) as a function of # edges ; as a function of k_optimal
 total_times = []
 finished_times = []
 finished_opt = []
 finished_densities = []
-
-
 
 for i in range(len(time_w)):
 	total_times.append(float(time_w[i]) + float(time_p[i]))
@@ -115,7 +118,6 @@ plt.xlim([-1,10])
 plt.ylim([-0.5,5])
 plt.savefig('time_from_density.png')
 
-
 # look at average runtime for non-trivial, non-timeouts
 nontt_times = 0.0
 div = 0
@@ -124,18 +126,29 @@ for i in range(len(k_opt)):
 		nontt_times = nontt_times + total_times[i]
 		div = div + 1
 
-print("(3) The average runtime of non-trivial, non-timeouts is: %f" % (nontt_times/div))
+print("(3) Number of non-trivial, non-timeouts is {}, ave runtime is {}".format(div, nontt_times/div))
 
 #look at distribution of gaps between lower_bound and k_optimal
 gaps = []
+cutsetgaps = []
 for i in range(len(k_opt)):
 	if(time_out[i] == 0):
-		gaps.append(int(k_opt[i]) - int(k_improved[i]))
+                gaps.append(int(k_opt[i]) - int(k_improved[i]))
+                cutsetgaps.append(int(k_opt[i]) - int(k_cutset[i]))
 
 print("(4) Distribution of gaps between lower_bound and k_optimal (gap-size: frequency):")
 print(Counter(gaps))
+print("Distribution of gaps between cutset and k_optimal (gap-size: frequency):")
+print(Counter(cutsetgaps))
 
-
+#scan for any instances where k_optimal != k_groundtruth
+print("(1) Instances where k-optimal != k-groundtruth:")
+counter = 0
+for i in range(len(k_opt)):
+	if k_opt[i] != k_gtrue[i] and time_out[i] == 0:
+            print("{} -- {} -- {} : {}, {}".format(files[i], instance_num[i], name[i], k_opt[i], k_gtrue[i]))
+            counter += 1
+print("{} instances where k-optimal < ground-truth".format(counter))
 
 
 
