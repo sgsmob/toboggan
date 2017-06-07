800f28caebef6a2627c92299a582d61cfa701f3a
Kyle Kloster <kyle.kloster@gmail.com>
2017-06-07 16:56:48 -0400

    still prepping to record catfish output

--- catfish-comparison/run_catfish_record_paths.py
+++ catfish-comparison/run_catfish_record_paths.py
@@ -6,9 +6,7 @@ import time
 
 
 def iterate_over_instances(graph_file,
-                           truth_file,
                            graph_file_name,
-                           truth_lookup,
                            results_file,
                            graphs_dir,
                            path_to_catfish,
@@ -26,66 +24,34 @@ def iterate_over_instances(graph_file,
             # clean up when not first iteration
             if tmp_file is not None:
                 tmp_file.close()
-                tmp_truth_file.close()
                 # run catfish on that instance
                 starttime = time.time()
                 num_paths = run_single_instance(tmp_file_name, path_to_catfish)
                 cattime = time.time() - starttime
-                # check if we got the optimal number of paths
-                check_if_optimal(num_paths,
-                                 true_num_paths,
-                                 results_file,
-                                 graph_file_name,
-                                 instance_name,
-                                 tmp_file_name,
-                                 tmp_truth_file_name,
-                                 graphs_dir)
                 os.remove(tmp_file_name)
-                os.remove(tmp_truth_file_name)
-                log_line = "{}\t{}\t{}\t{}\t{}\n".format(graph_file_name,
-                                                         instance_name,
-                                                         true_num_paths,
-                                                         num_paths,
-                                                         cattime)
+                # run catfish on that instance
+                starttime = time.time()
+                ofile = run_single_instance(tmp_file_name, path_to_catfish)
+                cattime = time.time() - starttime
+                log_line = "# {}\t{}\t{}\t{}\t{}\n".format(graph_file_name,
+                                                           instance_name,
+                                                           num_paths,
+                                                           cattime)
                 recording_log_file.write(log_line)
+                for ofile_line in ofile:
+                    recording_log_file.write(ofile_line)
+                ofile.close()
             # figure out the name of the instance
             instance_name = line.strip().split()[-1]
-            # count the number of paths in the corresponding segment of the
-            # truth file
-            true_num_paths = truth_lookup[instance_name]
             # make a temporary file for the input
             tmp_string = 'temp_working_file'
             tmp_file_name = tmp_string+".sgr"
             tmp_file = open(tmp_file_name, 'w')
 
-            tmp_truth_file_name = tmp_string+".sgrtruth"
-            tmp_truth_file = open(tmp_truth_file_name, 'w')
-            truth_write_flag = False
-            truth_file.seek(0)
-            # grabbing the .truth info for the graph instance
-            for truth_line in truth_file:
-                if truth_line[0] == '#':
-                    if truth_write_flag is True:
-                        break
-                    truth_instance = truth_line.strip().split()[-1]
-                    # print(instance_name)
-                    if truth_instance == instance_name:
-                        truth_write_flag = True
-                        # print(graph_file_name + ': flag is true\n')
-                    else:
-                        continue
-                else:
-                    if truth_write_flag is False:
-                        continue
-                    else:
-                        tmp_truth_file.write(truth_line)
-                        # print(truth_line)
-
         else:
             tmp_file.write(line)
 
     tmp_file.close()
-    tmp_truth_file.close()
 
     # run catfish on that instance
     starttime = time.time()
@@ -93,16 +59,14 @@ def iterate_over_instances(graph_file,
     cattime = time.time() - starttime
     log_line = "# {}\t{}\t{}\t{}\t{}\n".format(graph_file_name,
                                                instance_name,
-                                               true_num_paths,
                                                num_paths,
                                                cattime)
     recording_log_file.write(log_line)
-    for line in ofile:
-        recording_log_file.write(line)
+    for ofile_line in ofile:
+        recording_log_file.write(ofile_line)
     ofile.close()
 
     os.remove(tmp_file_name)
-    os.remove(tmp_truth_file_name)
 
 
 def run_single_instance(input_file_name, path_to_catfish):
@@ -144,15 +108,10 @@ def iterate_over_directory(input_dir,
         # open the graph file
         print("Processing file {}".format(f))
         graph_file = open(os.path.join(input_dir, f), 'r')
-        # the truth file has the same name but different extension
-        truth_file = open(os.path.join(input_dir, name+".truth"), 'r')
-        truth_lookup = create_truth_lookup(truth_file)
-        truth_file.close()
         # loop through all the instances in the opened files
         truth_file = open(os.path.join(input_dir, name+".truth"), 'r')
-        iterate_over_instances(graph_file, truth_file, name, truth_lookup,
-                               results_file, graphs_dir, path_to_catfish,
-                               recording_log_file)
+        iterate_over_instances(graph_file, name, results_file, graphs_dir,
+                               path_to_catfish, recording_log_file)
         graph_file.close()
         truth_file.close()
 
